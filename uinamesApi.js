1159001b8afac9515f65d06c6df3162cc6977f94
Hugo Larrousse <hugo.larrousse@chance.co>
2018-03-12 23:36:33 -0300

    more options available

--- uinamesApi.js
+++ uinamesApi.js
@@ -1,14 +1,16 @@
 const request = require('request-promise');
 const config = require('./config');
 
-const generateDate = async (size) => {
+const generateDate = async ({ size, gender, minlen, maxlen, region, ext }) => {
   try {
     const options = {
       uri :config.url,
       qs: {
         amount: size || config.defaultSize,
-      },
+      }
     };
+    Object.assign(options.qs, gender, minlen, maxlen, region, ext)
+    console.log('options :', options);
     const body = await request(options);
     if (body) {
       return JSON.parse(body);
